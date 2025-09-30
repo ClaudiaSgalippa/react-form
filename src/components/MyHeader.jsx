cc04ece2bc4c2cccecfd3d13b33455a834e8a865
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-30 15:54:10 +0200

    Impostazione Header & Main

---
+++ src/components/MyHeader.jsx
@@ -0,0 +1,14 @@
+const MyHeader = () => {
+
+    return (
+        <>
+            <header>
+                <div className="onlyHeader">
+                    <h1>Questo è l'header</h1>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default MyHeader
---
+++ src/components/MyMain.jsx
@@ -0,0 +1,14 @@
+const MyMain = () => {
+    
+    return (
+        <>
+            <main>
+                <div className="onlyHeader">
+                    <h1>Questo è il main</h1>
+                </div>
+            </main>
+        </>
+    )
+};
+
+export default MyMain
