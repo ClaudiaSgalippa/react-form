5d4a1696dac0ed2fe2cb3e6a605b62673266563e
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-30 16:18:50 +0200

    Creazione array con articoli

---
+++ src/components/MyArticles.jsx
@@ -0,0 +1,28 @@
+const MyArticles = [
+    {
+        id: 1,
+        title: "Il One Piece esiste veramente? Noi lo sappiamo!  - ESCLUSIVO",
+    },
+    {
+        id: 2,
+        title: "Tanjiro diveterà il pilastro del fuoco o dell'acqua? Lo scopriremo nel 2029. - INDISCREZIONI",
+    },
+    {
+        id: 3,
+        title: "Ma voi, l'avete VERAMENTE capito Attack on Titan? - REPORTAGE",
+    },
+    {
+        id: 4,
+        title: "Ma perchè i videogiochi ispirati agli anime fanno tutti schifo? - INCHIESTA",
+    },
+    {
+        id: 5,
+        title: "Epifania globale: le persone hanno realizzato che Sword Art Online è spazzatura - SCOOP MONDIALE",
+    },
+    {
+        id: 6,
+        title: "Lo strano rapporto fra Hisoka e Illumi ha avuto un'evoluzione? Scoop! - FOTO ESCLUSIVE",
+    }
+];
+
+export default MyArticles
