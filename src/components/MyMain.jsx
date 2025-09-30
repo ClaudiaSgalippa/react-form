6b2221f36947694490de833dd4738b91a6f3a7ba
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-30 16:51:54 +0200

    Inserimento dell'imput di text

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -1,10 +1,15 @@
+import MyArticles from "./MyArticles"; {/*Importiamo l'array di MyArticle.jsx nel MyMain.jsx*/}
+import {useState} from "react"; {/*Creazione della variabile di stato*/}
+
 const MyMain = () => {
+    
+    const [newArticles, setNewArticles] = useState(""); 
     
     return (
         <>
             <main>
-                <div className="onlyHeader">
-                    <h1>Questo è il main</h1>
+                <div className="onlyMain">
+                    <input type="text" placeholder="Nuovo articolo" value={newArticles} onChange={e => {setNewArticles(e.target.value)}}/>
                 </div>
             </main>
         </>
